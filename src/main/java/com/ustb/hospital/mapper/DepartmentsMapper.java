2c4626b931c6dc444c10b69fdc4f586f9987ed45
宋嘉仪 <14650464+S3201877355@user.noreply.gitee.com>
2024-07-07 20:07:08 +0800

    第三次

--- src/main/java/com/ustb/hospital/mapper/DepartmentsMapper.java
+++ src/main/java/com/ustb/hospital/mapper/DepartmentsMapper.java
@@ -6,4 +6,5 @@ import java.util.List;
 
 public interface DepartmentsMapper {
     List<Departments> selectAll();
+
 }
--- src/main/java/com/ustb/hospital/mapper/DoctorsMapper.java
+++ src/main/java/com/ustb/hospital/mapper/DoctorsMapper.java
@@ -5,5 +5,4 @@ import com.ustb.hospital.entity.Doctors;
 import java.util.List;
 
 public interface DoctorsMapper {
-    List <Doctors > selectAll();
-}
+    List <Doctors > selectAll();}
